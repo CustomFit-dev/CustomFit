fd1c9eceae8bfb604d7001c3fb20bddf93d55863
CustomFit-dev <custom.fit.team@gmail.com>
2024-06-14 11:31:49 -0500

    Index V1 finalizado
    Se creo la primera version mas parecida al diseño del index en figma y esta listo para ser mostrado, lo nuevo que tiene es el footer y la barra de navegadion bien diseñada

--- custom-fit/src/components/sec1.jsx
+++ custom-fit/src/components/sec1.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import Cam from "../img/Camisa-prueba.png";
 const Section = () => {
     return(
+      <div className="section-container">
+          <div className="overlay-div trapezoid-container">
+                <div className="trapezoid"></div>
+            </div>
         <section className='sec1'>
         <div className='container'>
           <div className='row'>
@@ -11,20 +14,14 @@ const Section = () => {
               <span className='highlight'>Personaliza</span> con <br /> Nosotros
             </h1>
             <div className='buttons'>
-              <button className='btn btn-primary'>Diseñar ya</button>
+              <button className='btn btn-outline-primary'>Diseñar ya</button>
               <button className='btn btn-secondary'>Tienda</button>
             </div>
-            </div>
-            <div className='col' id='col2'>
-              <div className='image-container' >
-                  <img src= {Cam} alt='Camisa custom-fit'/>
-              </div>
-
             </div>
           </div>
         </div>
       </section>
-
+      </div>
     )
 }
 
--- custom-fit/src/components/sec3.jsx
+++ custom-fit/src/components/sec3.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Cam from "../img/Camisa-prueba.png";
 import '../css/index.css';
 const Section3 = () => {
     return(
@@ -8,25 +7,21 @@ const Section3 = () => {
           <div className='row'>
             <div className='col'>
             <div className='image-container' id='img-c1'>
-                  <img src= {Cam} alt='Camisa custom-fit'/>
               </div>
               <h3>Camisetas</h3>
               </div>
               <div className='col'>
             <div className='image-container' id='img-c2'>
-                  <img src= {Cam} alt='Camisa custom-fit'/>
               </div>
               <h3>Gorras</h3>
               </div>
               <div className='col'>
             <div className='image-container' id='img-c3'>
-                  <img src= {Cam} alt='Camisa custom-fit'/>
               </div>
               <h3>Buzos</h3>
               </div>
-              <div className='col'>
+              <div className='col' id='col-4'>
             <div className='image-container' id='img-c4'>
-                  <img src= {Cam} alt='Camisa custom-fit'/>
               </div>
               <h3>Medias</h3>
             </div>
