b723af69f9f0f342493d191d030f6898792ec8fd
kevin patiño <151489788+kevin12daniel@users.noreply.github.com>
2024-06-20 11:18:56 -0500

    registro
    agregue los input

--- custom-fit/src/components/formulario.jsx
+++ custom-fit/src/components/formulario.jsx
@@ -3,15 +3,36 @@ import Sec1 from './sec1';
 import {BrowserRouter,Routes ,Route ,Link} from 'react-router-dom';
 const form = () => {
     return(
-       <div className='form'>
-            <Link className='btn btn-outline-primary' to="/cerrar"></Link>
-
-        <div className='container'>
-        <h1>Desde </h1>
+        <form>
+        <div className="form-row">
+        
+        <Link className='btn btn-outline-primary' to="/cerrar"></Link>
+        <div className="form-group col-md-6" id='input1'>
+                <input type="text" name="" id="" placeholder="Primer Nombre"/>
+                <input type="text" name="" id="" placeholder="Segundo Nombre"/>
         </div>
+        <div className="form-group col-md-6" id='input1'>
+                <input type="text" name="" id="" placeholder="Primer Apellido"/>
+                <input type="text" name="" id="" placeholder="Segundo Apellido"/>
+        </div>
+        <div className="form-group col-md-6" id='input1'>
+                <input type="text" name="" id="" placeholder="Nombre De Asuario"/>
+                <input type="text" name="" id="" placeholder="Direccion"/>
+        </div>
+        <div className="form-group col-md-6" id='input1'>
+                <input type="email" name="" id="" placeholder="Numero De Celular"/>
+                <input type="password" name="" id="" placeholder="Avater"/>
+        </div>
+        <div className="form-group col-md-6" id='input1'>
+                <input type="email" name="" id="" placeholder="Correo Electronico"/>
+                <input type="password" name="" id="" placeholder="Contraseña"/>
+        </div>
+
+    </div>
     
 
-        </div> 
+    <button type="submit" class="btn btn-primary">Sign in</button>
+    </form>
     )
 
 
