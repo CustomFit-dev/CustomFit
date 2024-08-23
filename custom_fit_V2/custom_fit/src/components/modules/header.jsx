2b66087471251076bd066f48e5415ea69a1e4ea1
CustomFit-dev <custom.fit.team@gmail.com>
2024-08-23 09:55:53 -0500

    Cambio de vinculaciones
    pequeño cambio en las vinculaciones

--- custom_fit_V2/custom_fit/src/components/modules/header.jsx
+++ custom_fit_V2/custom_fit/src/components/modules/header.jsx
@@ -13,7 +13,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import { Link } from 'react-router-dom';
 import logo from './mod_img/Logo-prin-f.png';
-import Form from './Registrar'; 
+import Form from './Registrar';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './Themes';
 
@@ -58,145 +58,144 @@ function Header() {
 
   return (
     <ThemeProvider theme={theme}>
-      <header>
-        <AppBar position="static" sx={{ zIndex: 1300 }}>
-          <Container maxWidth="xl">
-            <Toolbar disableGutters sx={{ zIndex: 1300 }}>
-              <Typography
-                variant="h6"
-                noWrap
-                component={Link}
-                to="/Home"
-                sx={{
-                  mr: 2,
-                  display: { xs: 'none', md: 'flex' },
-                  fontFamily: 'monospace',
-                  fontWeight: 700,
-                  letterSpacing: '.3rem',
-                  color: 'inherit',
-                  textDecoration: 'none',
-                }}
-              >
-                <img src={logo} alt="logo" id="logo" />
-              </Typography>
-
-              <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                <IconButton
-                  size="large"
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={handleOpenNavMenu}
-                  color="inherit"
-                >
-                  <MenuIcon />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorElNav}
-                  anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                  }}
-                  open={Boolean(anchorElNav)}
-                  onClose={handleCloseNavMenu}
+        <header>
+          <AppBar position="static" sx={{ zIndex: 1300 }}>
+            <Container maxWidth="xl">
+              <Toolbar disableGutters sx={{ zIndex: 1300 }}>
+                <Typography
+                  variant="h6"
+                  noWrap
+                  component={Link}
+                  to="/Home"
                   sx={{
-                    display: { xs: 'block', md: 'none' },
+                    mr: 2,
+                    display: { xs: 'none', md: 'flex' },
+                    fontFamily: 'monospace',
+                    fontWeight: 700,
+                    letterSpacing: '.3rem',
+                    color: 'inherit',
+                    textDecoration: 'none',
                   }}
                 >
-                  {pages.map((page) => (
-                    <MenuItem key={page.name} onClick={handleCloseNavMenu}>
-                      <Typography textAlign="center">
-                        <Link to={page.route} style={{ textDecoration: 'none', color: 'inherit' }}>
-                          {page.name}
-                        </Link>
-                      </Typography>
-                    </MenuItem>
-                  ))}
-                </Menu>
-              </Box>
-              <Typography
-                variant="h5"
-                noWrap
-                component={Link}
-                to="/Home" // Cambié el 'to' a '/Home' aquí también
-                sx={{
-                  mr: 2,
-                  display: { xs: 'flex', md: 'none' },
-                  flexGrow: 1,
-                  fontFamily: 'monospace',
-                  fontWeight: 700,
-                  letterSpacing: '.3rem',
-                  color: 'inherit',
-                  textDecoration: 'none',
-                }}
-              >
-                <img src={logo} alt="logo" id="logo" />
-              </Typography>
-              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end', marginRight: 2 }}>
-                {pages.map((page) => (
-                  <Button
-                    key={page.name}
-                    component={Link} // Cambié 'component="a"' a 'component={Link}'
-                    to={page.route}
-                    sx={{ my: 2, color: 'white', display: 'block' }}
+                  <img src={logo} alt="logo" id="logo" />
+                </Typography>
+
+                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                  <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleOpenNavMenu}
+                    color="inherit"
                   >
-                    {page.name}
-                  </Button>
-                ))}
-              </Box>
-
-              <Box sx={{ flexGrow: 0 }}>
-                <Tooltip title="Open settings">
-                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Avatar alt="Remy Sharp" />
+                    <MenuIcon />
                   </IconButton>
-                </Tooltip>
-                <Menu
-                  sx={{ mt: '45px' }}
-                  id="menu-appbar"
-                  anchorEl={anchorElUser}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorElNav}
+                    anchorOrigin={{
+                      vertical: 'bottom',
+                      horizontal: 'left',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'left',
+                    }}
+                    open={Boolean(anchorElNav)}
+                    onClose={handleCloseNavMenu}
+                    sx={{
+                      display: { xs: 'block', md: 'none' },
+                    }}
+                  >
+                    {pages.map((page) => (
+                      <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">
+                          <a href={page.route} style={{ textDecoration: 'none', color: 'inherit' }}>
+                            {page.name}
+                          </a>
+                        </Typography>
+                      </MenuItem>
+                    ))}
+                  </Menu>
+                </Box>
+                <Typography
+                  variant="h5"
+                  noWrap
+                  component={Link}
+                  to="/Home"
+                  sx={{
+                    mr: 2,
+                    display: { xs: 'flex', md: 'none' },
+                    flexGrow: 1,
+                    fontFamily: 'monospace',
+                    fontWeight: 700,
+                    letterSpacing: '.3rem',
+                    color: 'inherit',
+                    textDecoration: 'none',
                   }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={Boolean(anchorElUser)}
-                  onClose={handleCloseUserMenu}
                 >
-                  {settings.map((setting) => (
-                    <MenuItem key={setting.name} onClick={() => { handleCloseUserMenu(); handleOpenForm(); }}>
-                      <Typography textAlign="center">
-                        <a href="#" style={{ textDecoration: 'none', color: 'black' }}>
-                          {setting.name}
-                        </a>
-                      </Typography>
-                    </MenuItem>
+                  <img src={logo} alt="logo" id="logo" />
+                </Typography>
+                <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end', marginRight: 2 }}>
+                  {pages.map((page) => (
+                    <Button
+                      key={page.name}
+                      href={page.route}
+                      sx={{ my: 2, color: 'white', display: 'block' }}
+                    >
+                      {page.name}
+                    </Button>
                   ))}
-                </Menu>
-              </Box>
-            </Toolbar>
-          </Container>
-        </AppBar>
-        {isFormVisible && (
-          <div className="overlay">
-            <div className="form-container">
-              <Form onClose={handleCloseForm} />
-            </div>
-          </div>
-        )}
-      </header>
-    </ThemeProvider>
-  );
-}
+                </Box>
 
-export default Header;
+                <Box sx={{ flexGrow: 0 }}>
+                  <Tooltip title="Open settings">
+                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                      <Avatar alt="Remy Sharp" />
+                    </IconButton>
+                  </Tooltip>
+                  <Menu
+                    sx={{ mt: '45px' }}
+                    id="menu-appbar"
+                    anchorEl={anchorElUser}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    open={Boolean(anchorElUser)}
+                    onClose={handleCloseUserMenu}
+                  >
+                    {settings.map((setting) => (
+                      <MenuItem key={setting.name} onClick={() => { handleCloseUserMenu(); handleOpenForm(); }}>
+                        <Typography textAlign="center">
+                          <a href="#" style={{ textDecoration: 'none', color: 'black' }}>
+                            {setting.name}
+                          </a>
+                        </Typography>
+                      </MenuItem>
+                    ))}
+                  </Menu>
+                </Box>
+              </Toolbar>
+            </Container>
+          </AppBar>
+          {isFormVisible && (
+            <div className="overlay">
+              <div className="form-container">
+                <Form onClose={handleCloseForm} />
+              </div>
+            </div>
+          )}
+        </header>
+      </ThemeProvider>
+    );
+  }
+
+  export default Header;
