1afa042765d364400f198c8871e8e74624926dcf
CustomFit-dev <custom.fit.team@gmail.com>
2024-08-27 11:17:02 -0500

    Mejoras en carrousel
    Se implemento una nueva imagen y mejoras al carroursel de la tienda

--- custom_fit_V2/custom_fit/src/components/modules/carrosel.jsx
+++ custom_fit_V2/custom_fit/src/components/modules/carrosel.jsx
@@ -8,7 +8,7 @@ import 'swiper/css/autoplay';
 import { Pagination, Autoplay } from 'swiper/modules';
 
 import img1 from './mod_img/Sto-1.png'; 
-import img2 from './mod_img/nav.png'; 
+import img2 from './mod_img/Sto-2.png'; 
 import img3 from './mod_img/nav.png'; 
 
 export default function Carso() {
@@ -28,7 +28,7 @@ export default function Carso() {
         <img src={img1} id="c-1" alt="Slide 1" />
       </SwiperSlide>
       <SwiperSlide>
-        <img src={img2} id="c-1" alt="Slide 2" />
+        <img src={img2} id="c-2" alt="Slide 2" />
       </SwiperSlide>
       <SwiperSlide>
         <img src={img3} id="c-1" alt="Slide 3" />
