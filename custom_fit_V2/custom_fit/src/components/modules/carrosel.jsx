3c8b5b241f914599d21659318063f268869ee286
alan-sg17 <163859193+alan-sg17@users.noreply.github.com>
2024-10-24 11:19:58 -0500

    carrusel
    se agrego igaes nuevas

--- custom_fit_V2/custom_fit/src/components/modules/carrosel.jsx
+++ custom_fit_V2/custom_fit/src/components/modules/carrosel.jsx
@@ -1,31 +1,32 @@
 // Carso.js
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/autoplay';
 
+
 import { Pagination, Autoplay } from 'swiper/modules';
 
-import img1 from './mod_img/car1.png'; 
-import img2 from './mod_img/car2.png'; 
-import img3 from './mod_img/car3.png'; 
+import img1 from '../../img/slider2.png'; 
+import img2 from '../../img/slider1.png'; 
+import img3 from '../../img/slider3.png'; 
+import img4 from '../../img/slider4.png';
 
 export default function Carso() {
   return (
-    <div style={{ overflow: 'hidden' }}>
+    <div className="carousel">
       <Swiper
         pagination={{
           dynamicBullets: true,
         }}
         autoplay={{
-          delay: 6000, // Cambia este valor para ajustar el tiempo de visualización de cada imagen (5000 ms = 5 segundos)
+          delay: 6000,
           disableOnInteraction: false,
         }}
         modules={[Pagination, Autoplay]}
         className="mySwiper"
-        style={{ width: '100%', height: '90vh', marginLeft: '-10px' }}
+        style={{ width: '100%', height: '100%' }}
       >
         <SwiperSlide>
           <img src={img1} id="c-1" alt="Slide 1" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
@@ -35,6 +36,9 @@ export default function Carso() {
         </SwiperSlide>
         <SwiperSlide>
           <img src={img3} id="c-3" alt="Slide 3" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img4} id="c-4" alt="Slide 4" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
         </SwiperSlide>
       </Swiper>
     </div>
