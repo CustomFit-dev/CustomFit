bafc6a6cbc359e86b81b548c2bdbbe1ca847baf5
kevin patiño <kevin11234daniel@gmail.com>
2024-11-12 19:42:21 -0500

    modificacion de dashboard
    se agrego nuevos caomps

--- custom_fit_V2/custom_fit/src/components/modules/profile.jsx
+++ custom_fit_V2/custom_fit/src/components/modules/profile.jsx
@@ -3,39 +3,58 @@ import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { createTheme, Experimental_CssVarsProvider, experimental_extendTheme } from '@mui/material/styles';
-import DashboardIcon from '@mui/icons-material/Dashboard';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { AppProvider } from '@toolpad/core/AppProvider';
 import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+import PersonIcon from '@mui/icons-material/Person';
+import LockClockIcon from '@mui/icons-material/LockClock';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import LockIcon from '@mui/icons-material/Lock';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import PaymentIcon from '@mui/icons-material/Payment';
+import LogoutIcon from '@mui/icons-material/Logout';
+import Logo from '../../img/logocustomfit.png';
+
 
 const NAVIGATION = [
   {
-    segment: 'dashboard',
-    title: 'Dashboard',
-    icon: <DashboardIcon />,
+    segment: 'Datos personales',
+    title: 'Datos personales',
+    icon: <PersonIcon />,
   },
   {
-    segment: 'orders',
-    title: 'Orders',
-    icon: <ShoppingCartIcon />,
+    segment: 'Control de actividad',
+    title: 'Control de actividad',
+    icon: <LockClockIcon />,
+  },
+  {
+    segment: 'Historial de compras',
+    title: 'Historial de compras',
+    icon: <ShoppingBagIcon />,
+  },
+  {
+    segment: 'Seguiridad',
+    title: 'Seguiridad',
+    icon: <LockIcon />,
+  },
+  {
+    segment: 'Seguiridad',
+    title: 'Mis pedidos',
+    icon: <LocalShippingIcon />,
+  },
+  {
+    segment: 'Pagos',
+    title: 'Pagos',
+    icon: <PaymentIcon />,
+  },
+  {
+    segment: 'Salir',
+    title: 'Salir',
+    icon: <LogoutIcon />,
   },
 ];
 
-const demoTheme = experimental_extendTheme({
-  cssVariables: {
-    colorSchemeSelector: 'data-toolpad-color-scheme',
-  },
-  colorSchemes: { light: true, dark: true },
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 600,
-      md: 600,
-      lg: 1200,
-      xl: 1536,
-    },
-  },
-});
+
 
 function DemoPageContent({ pathname }) {
   return (
@@ -73,15 +92,15 @@ function Profile(props) {
   const demoWindow = window !== undefined ? window() : undefined;
 
   return (
-    <Experimental_CssVarsProvider theme={demoTheme}>
+    <Experimental_CssVarsProvider>
       <AppProvider
         navigation={NAVIGATION}
         branding={{
-          logo: <img src="https://mui.com/static/logo.png" alt="MUI logo" />,
-          title: 'MUI',
+          logo: <img src={Logo} alt="MUI logo" />,
+          title: 'CustomFit',
         }}
         router={router}
-        theme={demoTheme}
+        
         window={demoWindow}
       >
         <DashboardLayout>
