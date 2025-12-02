a157510c82fb9571b4a12d78a6dd41833912a6cb
4kl4m1sk <juanjosegamer19852@gmail.com>
2025-12-02 06:53:46 -0500

    reparacion
    asd

--- custom_fit_V2/custom_fit/src/index.js
+++ custom_fit_V2/custom_fit/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './scss/style.scss';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // Cambiado BrowserRouter por HashRouter
 import { AuthProvider } from './components/modules/authcontext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
